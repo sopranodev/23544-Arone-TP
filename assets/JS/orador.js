4e53197d2e0fbc225aa044e547e52e53f1a4e005
sopranodev <56509393+sopranodev@users.noreply.github.com>
2023-12-06 15:37:42 -0300

    Se agregó clase 05/12

--- assets/JS/orador.js
+++ assets/JS/orador.js
@@ -30,16 +30,16 @@ function formularioCompleto() {
         correo.focus();
         return false;
     }
-    if (titulo.value === "") {
+    if (tema.value === "") {
         alert("Por favor, escriba su título del tema");
-        titulo.classList.add("is-invalid");
-        titulo.focus();
+        tema.classList.add("is-invalid");
+        tema.focus();
         return false;
     }
-    if (descripcion.value === "") {
+    if (resumen.value === "") {
         alert("Por favor, escriba su resumen");
-        descripcion.classList.add("is-invalid");
-        descripcion.focus();
+        resumen.classList.add("is-invalid");
+        resumen.focus();
         return false;
     }
 }
@@ -50,19 +50,34 @@ btn - enviar.addEventListener('click', function () {
         const nombre = document.getElementById('nombre').value;
         const apellido = document.getElementById('apellido').value;
         const email = document.getElementById('email').value;
-        const titulo = document.getElementById('titulo').value;
-        const descripcion = document.getElementById('descripcion').value;
+        const tema = document.getElementById('tema').value;
+        const resumen = document.getElementById('resumen').value;
         const mensaje = `Hola ${nombre} ${apellido}!\nUsted enviará los siguientes datos: su Correo es ${email}.\nLa charla que dará se titula ${titulo} \n\n Su resumen es: ${descripcion}`;
         alert(`${mensaje}`);
     }
 });
+//Crear orador
+crearOperador = () => {
+    const orador = {
+        nombre: document.getElementById("nombre").value,
+        apellido: document.getElementById("apellido").value,
+        email: document.getElementById("email").value,
+        tema: document.getElementById("tema").value,
+        resumen: document.getElementById("resumen").value
+    };
 
-document.getElementById("orador").addEventListener("submit", function (event) {
-    event.preventDefault();
-    if (formularioCompleto()) {
-
-    }
-});
+    //debo enviar estos datos al sevidor
+    fetch(`http://localhost:8080/c23544-Arone-Back/api/orador`, {
+        method: "POST",
+        body: JSON.stringify(orador),
+    })
+        .then(response => response.json())
+        .then(json => {
+            alert(`alta de orador id:${json.id}] ok`);
+        })
+        .catch(err => console.log(err));
+}
+document.getElementById("btnCrear").addEventListener('click', crearOperador);
 
 //Función para remover los invalid de los inputs
 function removeClassError() {
@@ -71,4 +86,67 @@ function removeClassError() {
     for (let i = 0; i < inputs.length; i++) {
         inputs[i].classList.remove("is-invalid");
     }
+}
+
+//Listado
+function listarOradores() {/*f2*/
+    //1 preparar
+    const respuesta = fetch(`http://localhost:8080/web-app-23544/api/orador`);
+
+    //2 invocar
+    respuesta
+        .then(response => response.json())
+        .then(data => procesarListado(data))//fulfilled
+        .catch(error => dibujarError(error))//rejected
+}
+function procesarListado(data) {
+    const listarOradores = data;
+    let rows = '';
+    for (let orador of listarOradores) {
+        console.log(orador);
+        rows += `
+                <tr>
+                    <th scope="row">${orador.id}</th>
+                    <td>${orador.nombre}</td>
+                    <td>${orador.apellido}</td>
+                    <td>${orador.mail}</td>
+                    <td>${orador.tema}</td>
+                    <td>${orador.resumen}</td>
+                    <td>
+                        <button>Editar</button>
+                        <button onclick="eliminarOrador(${orador.id})">
+                            Eliminar
+                        </button>
+                    </td>
+                </tr>
+                `
+    }
+    document.getElementById('usersRows').innerHTML = rows;
+}
+
+function dibujarError(error) {
+    console.log(error);
+    const alerta = `<div class="alert alert-danger" role="alert">
+                ${error.toString()}
+            </div>`;
+    document.getElementById('msj').innerHTML = alerta;
+}
+
+document.getElementById('btnGetUsers').addEventListener('click', listarOradores);
+
+//eliminar
+eliminarOrador = (id) => {
+    if (!confirm('SEGURO?')) {
+        return;
+    }
+
+    fetch(`http://localhost:8080/web-app-23544/api/orador?id=${id}`, {
+        method: "DELETE",
+    })
+        .then(response => response)
+        .then(json => {
+            alert(`se ha eliminado el orador id: ${id}`);
+            listarOradores();
+        })
+        .catch(err => console.log(err));
 }
