4f1f8bff11c9ec6ee0fe920782c11f44de2ad503
CeSoprano <56509393+sopranodev@users.noreply.github.com>
2023-12-11 23:22:50 -0300

    Se agregó listar oradores y botones

--- assets/JS/orador.js
+++ assets/JS/orador.js
@@ -61,7 +61,7 @@ crearOrador = () => {
     const orador = {
         nombre: document.getElementById("nombre").value,
         apellido: document.getElementById("apellido").value,
-        mail: document.getElementById("mail").value,
+        email: document.getElementById("mail").value,
         tema: document.getElementById("tema").value,
         resumen: document.getElementById("resumen").value
     };
@@ -73,7 +73,7 @@ crearOrador = () => {
     })
         .then(response => response.json())
         .then(json => {
-            alert(`alta de orador id:${json.id}] ok`);
+            alert(`alta de orador id:${json.id} ok`);
         })
         .catch(err => console.log(err));
 }
@@ -99,7 +99,10 @@ function listarOradores() {/*f2*/
         .then(data => procesarListado(data))//fulfilled
         .catch(error => dibujarError(error))//rejected
 }
+
 function procesarListado(data) {
+    //guardo en localStorage
+    saveOradoresInFromLocal('oradores', data);
     const listarOradores = data;
     let rows = '';
     for (let orador of listarOradores) {
@@ -113,8 +116,12 @@ function procesarListado(data) {
                     <td>${orador.tema}</td>
                     <td>${orador.resumen}</td>
                     <td>
-                        <button>Editar</button>
-                        <button onclick="eliminarOrador(${orador.id})">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-enviar" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="editar(${orador.id})">
+                      Editar
+                    </button>
+                    
+                        <button onclick="eliminarOrador(${orador.id}) type="button" class= "btn btn-borrar" data-bs-toggle="modal" data-bs-target="#exampleModal"">
                             Eliminar
                         </button>
                     </td>
@@ -149,4 +156,82 @@ eliminarOrador = (id) => {
             listarOradores();
         })
         .catch(err => console.log(err));
+}
+
+//Actualizar
+const getOradoresFromLocal = () => {
+    const oradores = localStorage.getItem('oradores')
+    if (oradores) {
+        return JSON.parse(oradores);
+    }
+    return [];
+}
+const getOradorSeleccionado = () => {
+    const obj = localStorage.getItem('oradorBuscado')
+    if (obj) {
+        return JSON.parse(obj);
+    }
+    return null;
+}
+const removeOradorSeleccionado = () => {
+    localStorage.removeItem('oradorBuscado');
+}
+
+const saveOradoresInFromLocal = (key, data) => {
+    localStorage.setItem(key, JSON.stringify(data));//como texto
+}
+
+const editar = (id) => {
+    const oradores = getOradoresFromLocal();//[]
+    const oradorBuscado = oradores.find(o => o.id === id);
+
+    //si quiero actualizar algo en un html (.innerHTML o .value)
+    document.getElementById('nombreActualizar').value = oradorBuscado.nombre;
+    document.getElementById('apellidoActualizar').value = oradorBuscado.apellido;
+    document.getElementById('mailActualizar').value = oradorBuscado.mail;
+    document.getElementById('temaActualizar').value = oradorBuscado.tema;
+    document.getElementById('resumenActualizar').value = oradorBuscado.resumen;
+
+    //guardo el id/orador del orador que se quiere actualizar
+    saveOradoresInFromLocal('oradorBuscado', oradorBuscado);
+}
+
+const actualizarOrador = () => {
+    const oradorSeleccionado = getOradorSeleccionado();
+    if (!oradorSeleccionado) {
+        return
+    }
+
+    //obtengo los datos del formulario que esta en el modal
+    const nombre = document.getElementById('nombreActualizar').value;
+    const apellido = document.getElementById('apellidoActualizar').value;
+    const email = document.getElementById('mailActualizar').value;
+    const tema = document.getElementById('temaActualizar').value;
+    const resumen = document.getElementById('resumenActualizar').value;
+
+    const orador = {
+        nombre,
+        apellido,
+        email,
+        tema,
+        resumen
+    };
+    //ahora puedo enviar al backend para actualizar
+    //debo enviar estos datos al sevidor: https://www.freecodecamp.org/espanol/news/tutorial-de-fetch-api-en-javascript-con-ejemplos-de-js-fetch-post-y-header/
+    fetch(`http://localhost:8080/web-app-23544/api/orador?id=${oradorSeleccionado.id}`, {
+        method: "PUT",
+        body: JSON.stringify(orador),
+    })
+        .then(response => response) //status code 200
+        .then(json => {
+            alert(`Se ha modificado el orador id:${oradorSeleccionado.id}`);
+            //cargar la lista 
+            listarOradores();
+            removeOradorSeleccionado();
+            cerrarModal();
+        })
+        .catch(err => console.log(err));
+}
+const cerrarModal = () => {
+    document.getElementById('btnCerrarModal').click();
 }
